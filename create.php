19899b5e2de960690361b0d79f25f9f07f3a12ef
Papendiogouniang <142728665+Papendiogouniang@users.noreply.github.com>
2023-11-23 12:36:14 +0100

    Update create.php

--- create.php
+++ create.php
@@ -2,33 +2,33 @@
 $servername= "localhost";
 $username = "root";
 $password ="";  
-$database="niangshop";
+$database="crud";
 //Create connection
 $connection = new mysqli($servername, $username, $password, $database);
 
 
-$name = "";
-$email = "";
-$phone = "";
-$address = "";
+$code = "";
+$nom = "";
+$description = "";
+$budget= "";
 
 $errorMessage="";
 $successMessage="";
 if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
-    $name =$_POST["name"];
-    $email =$_POST["email"];
-    $phone =$_POST["phone"];
-    $address =$_POST["address"];
+    $code =$_POST["code"];
+    $nom =$_POST["nom"];
+    $description =$_POST["description"];
+    $budget =$_POST["budget"];
 
     do{
-        if (empty($name) || empty($email) || empty($phone) || empty($address)) {
+        if (empty($code) || empty($nom) || empty($description) || empty($budget)) {
             $errorMessage = "All the fields are required";
             break;
         }
 
         //add new client to database
-        $sql = "INSERT INTO clients (name,email,phone,address)" . 
-        "VALUES ('".$name."','". $email. "', '".$phone."' , '" .$address. "' ) ";
+        $sql = "INSERT INTO projet (code,nom,description,budget)" . 
+        "VALUES ('".$code."','". $nom. "', '".$description."' , '" .$budget. "' ) ";
         $result = $connection->query($sql);
 
         if (!$result) {
@@ -36,14 +36,14 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
             break;
         }
 
-        $name = "";
-        $email = "";
-        $phone = "";
-        $address = "";
+        $code = "";
+        $nom = "";
+        $description = "";
+        $budget = "";
 
-        $successMessage = "Client added correctly";
+        $successMessage = "Projet added correctly";
 
-        header("location: /niangshop/index.php");
+        header("location: /crud/index.php");
         exit;
     } while (false);
 }
@@ -55,13 +55,13 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Niang shop</title>
+    <title>crud</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     <div class="container my-5">
-        <h2>New client</h2>
+        <h2>New projet</h2>
 
          <?php
          if (!empty($errorMessage)){
@@ -76,36 +76,36 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
 
         <form method="post">
             <div class="row mb-3">
-                <label class="col-sm-3 col-form-label">Name</label>
+                <label class="col-sm-3 col-form-label">Code</label>
                 <div class="col-sm-6">
-                    <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
+                    <input type="number" class="form-control" name="code" value="<?php echo $code; ?>">
 
                 </div>
 
             </div>
 
             <div class="row mb-3">
-                <label class="col-sm-3 col-form-label">Email</label>
+                <label class="col-sm-3 col-form-label">Nom</label>
                 <div class="col-sm-6">
-                    <input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
+                    <input type="text" class="form-control" name="nom" value="<?php echo $nom; ?>">
 
                 </div>
 
             </div>
 
             <div class="row mb-3">
-                <label class="col-sm-3 col-form-label">Phone</label>
+                <label class="col-sm-3 col-form-label">Description</label>
                 <div class="col-sm-6">
-                    <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
+                    <input type="text" class="form-control" name="description" value="<?php echo $description; ?>">
 
                 </div>
 
             </div>
 
             <div class="row mb-3">
-                <label class="col-sm-3 col-form-label">Address</label>
+                <label class="col-sm-3 col-form-label">Budget</label>
                 <div class="col-sm-6">
-                    <input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
+                    <input type="number" class="form-control" name="budget" value="<?php echo $budget; ?>">
 
                 </div>
 
@@ -134,7 +134,7 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST'){
 
                 </div>
                 <div class="col-sm-3 d-grid">
-                    <a class="btn btn-outline-primary" href="/niangshop/index.php" role="button">Cancel</a>
+                    <a class="btn btn-outline-primary" href="/crud/index.php" role="button">Cancel</a>
 
                 </div>
 
